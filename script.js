c32ca3454d5f54470ba19d2da216b52c264d3155
Ryan Oddi <ryanoddi@gmail.com>
2021-02-15 12:28:45 -0500

    started over, commented out old stuff incase. new approach as code was becoming unbearable to figure out next steps

--- script.js
+++ script.js
@@ -1,49 +1,91 @@
-function add(a, b) {
-  return +(parseInt(a) + parseInt(b));
-}
-// console.log(add(5, 4));
+const storeVal = {
+  displayValue: "0",
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
 
-function subtract(a, b) {
-  return a - b;
-}
-// console.log(subtract(5, 4));
-
-function multiply(a, b) {
-  return a * b;
-}
-// console.log(multiply(5, 4));
-
-function divide(a, b) {
-  return a / b;
-}
-// console.log(divide(5, 4));
-
-function operate(operator) {
-  // a = prompt("num1", "0");
-  operator = document.getElementById("add").value;
-  // b = prompt("num2", "0");
-  if (operator === "/") return divide(a, b);
-  if (operator === "+") return add(a, b);
-  if (operator === "-") return subtract(a, b);
-  if (operator === "*") return multiply(a, b);
+function update() {
+  const display = document.querySelector(".calcDisplay");
+  display.value = storeVal.displayValue;
 }
 
-// function to insert button click to input field
+update();
 
-function insert(button) {
-  let val = button.value;
-  document.getElementById("input").value += val;
-  // console.log(insert(button));
-}
-
-// Function allows user to input via keyboard with prompts
-// function operate(operator, a, b) {
-//   a = prompt("num1", "0");
-//   operator = prompt("choose one operator", "+, -, *, or /");
-//   b = prompt("num2", "0");
-//   if (operator === "/") return divide(a, b);
-//   if (operator === "+") return add(a, b);
-//   if (operator === "-") return subtract(a, b);
-//   if (operator === "*") return multiply(a, b);
+const butts = document.querySelector(".calcButts");
+butts.addEventListener("click", (event) => {
+  const { target } = event;
+  if (!target.matches("button")) {
+    return;
+  }
+  if (target.classList.contains("operator")) {
+    console.log("operator", target.value);
+    return;
+  }
+  if (target.classList.contains("decimal")) {
+    console.log("decimal", target.value);
+    return;
+  }
+  if (target.classList.contains("allClear")) {
+    console.log("clear", target.value);
+    return;
+  }
+  console.log("digit", target.value);
+});
+// function add(a, b) {
+//   return +(parseInt(a) + parseInt(b));
 // }
-// console.log(operate());
+// // console.log(add(5, 4));
+
+// function subtract(a, b) {
+//   return a - b;
+// }
+// // console.log(subtract(5, 4));
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+// // console.log(multiply(5, 4));
+
+// function divide(a, b) {
+//   return a / b;
+// }
+// // console.log(divide(5, 4));
+
+// // function to insert button click to input field
+
+// function insert(button) {
+//   let val = button.value;
+//   document.getElementById("input").value += val;
+//   // console.log(insert(button));
+// }
+
+// function storeVal() {
+//   let input = document.getElementById("input").value,
+//     findOp = /\*|\+|-|\//g,
+//     match;
+//   while ((match = findOp.exec(input))) {
+//     console.log(match);
+//   }
+//   firstNum = input.substring(0, match);
+//   secondNum = input.substring(match + 1);
+//   console.log(firstNum);
+//   console.log(secondNum);
+// }
+
+// function operate(a, operator, b) {
+//   a = Number(a);
+//   b = Number(b);
+//   switch (operator) {
+//     case "+":
+//       return add(a, b);
+//     case "-":
+//       return subtract(a, b);
+//     case "*":
+//       return multiply(a, b);
+//     case "/":
+//       if (b === 0) return null;
+//       else return divide(a, b);
+//     default:
+//       return null;
+//   }
