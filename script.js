726abf8e041f3a3eed47183a20ac255d8c4a671f
Ryan Oddi <ryanoddi@gmail.com>
2021-02-15 15:23:29 -0500

    display updates with new calculated operand value, or when user presses =

--- script.js
+++ script.js
@@ -5,6 +5,54 @@ const storeVal = {
   operator: null,
 };
 
+function selectDigit(digit) {
+  const { displayVal, waitingForSecondOperand } = storeVal;
+
+  if (waitingForSecondOperand === true) {
+    storeVal.displayVal = digit;
+    storeVal.waitingForSecondOperand = false;
+  } else {
+    storeVal.displayVal = displayVal === "0" ? digit : displayVal + digit;
+  }
+  console.log(storeVal);
+}
+
+function selectDec(decimal) {
+  if (!storeVal.displayVal.includes(decimal)) {
+    storeVal.displayVal += decimal;
+  }
+}
+
+function selectOp(nextOperator) {
+  const { firstOperand, displayVal, operator } = storeVal;
+  const inputVal = parseFloat(displayVal);
+
+  if (firstOperand == null && !isNaN(inputVal)) {
+    storeVal.firstOperand = inputVal;
+  } else if (operator) {
+    const result = calc(firstOperand, inputVal, operator);
+
+    storeVal.displayVal = String(result);
+    storeVal.firstOperand = result;
+  }
+  storeVal.waitingForSecondOperand = true;
+  storeVal.operator = nextOperator;
+  console.log(storeVal);
+}
+
+function calc(firstOperand, secondOperand, operator) {
+  if (operator === "+") {
+    return firstOperand + secondOperand;
+  } else if (operator === "-") {
+    return firstOperand - secondOperand;
+  } else if (operator === "/") {
+    return firstOperand / secondOperand;
+  } else if (operator === "*") {
+    return firstOperand * secondOperand;
+  }
+  return secondOperand;
+}
+
 function update() {
   const display = document.querySelector(".calcDisplay");
   display.value = storeVal.displayVal;
@@ -35,89 +83,3 @@ butts.addEventListener("click", (event) => {
   selectDigit(target.value);
   update();
 });
-
-function selectDigit(digit) {
-  const { displayVal, waitingForSecondOperand } = storeVal;
-
-  if (waitingForSecondOperand === true) {
-    storeVal.displayVal = digit;
-    storeVal.waitingForSecondOperand = false;
-  } else {
-    storeVal.displayVal = displayVal === "0" ? digit : displayVal + digit;
-  }
-  console.log(storeVal);
-}
-
-function selectDec(decimal) {
-  if (!storeVal.displayVal.includes(decimal)) {
-    storeVal.displayVal += decimal;
-  }
-}
-
-function selectOp(nextOperator) {
-  const { firstOperand, displayVal, operator } = storeVal;
-  const inputVal = parseFloat(displayVal);
-  if (firstOperand === null && !isNaN(inputVal)) {
-    storeVal.firstOperand = inputVal;
-  }
-  storeVal.waitingForSecondOperand = true;
-  storeVal.operator = nextOperator;
-  console.log(storeVal);
-}
-// function add(a, b) {
-//   return +(parseInt(a) + parseInt(b));
-// }
-// // console.log(add(5, 4));
-
-// function subtract(a, b) {
-//   return a - b;
-// }
-// // console.log(subtract(5, 4));
-
-// function multiply(a, b) {
-//   return a * b;
-// }
-// // console.log(multiply(5, 4));
-
-// function divide(a, b) {
-//   return a / b;
-// }
-// // console.log(divide(5, 4));
-
-// // function to insert button click to input field
-
-// function insert(button) {
-//   let val = button.value;
-//   document.getElementById("input").value += val;
-//   // console.log(insert(button));
-// }
-
-// function storeVal() {
-//   let input = document.getElementById("input").value,
-//     findOp = /\*|\+|-|\//g,
-//     match;
-//   while ((match = findOp.exec(input))) {
-//     console.log(match);
-//   }
-//   firstNum = input.substring(0, match);
-//   secondNum = input.substring(match + 1);
-//   console.log(firstNum);
-//   console.log(secondNum);
-// }
-
-// function operate(a, operator, b) {
-//   a = Number(a);
-//   b = Number(b);
-//   switch (operator) {
-//     case "+":
-//       return add(a, b);
-//     case "-":
-//       return subtract(a, b);
-//     case "*":
-//       return multiply(a, b);
-//     case "/":
-//       if (b === 0) return null;
-//       else return divide(a, b);
-//     default:
-//       return null;
-//   }
